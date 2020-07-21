2309dd21f0014d1cf02cb4311442c6f4debdd699
Minudharshini23 <55809285+Minudharshini23@users.noreply.github.com>
2020-07-21 21:04:31 +0530

    Update sol_3.js

--- DAY 5/scripts/sol_3.js
+++ DAY 5/scripts/sol_3.js
@@ -1,18 +1,13 @@
-//ASYNC AND AWAIT
-
-const days = [];
-async function Weekdays(){
-    let status = new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            let n=prompt("enter the range:");
-            for(let i=0;i<n;i++)
-            days.push(prompt("enter the days:"));
-            resolve('days are listed');
-        },3000)
-    })
-
-    let result = await status;
-    console.log(days);
+function fetchTodo(){
+    fetch("https://jsonplaceholder.typicode.com/todos").then(response=>{console.log(response)
+     return response.json()}).then(data=>{
+         console.log(data);
+         for(let i=0;i<data.length;i++){
+             if(data[i].completed==true){
+                 console.log(data[i]);
+             }
+         }
+        })
+     .catch(error=>console.log(error))
 }
-
-Weekdays();
+fetchTodo();
